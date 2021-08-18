7726fc9c567adc70c42e7c4c6e40d5eefc046ec7
Anh Tong <tongkimanh@yahoo.com>
2021-08-18 03:43:08 +0000

    update

--- play.js
+++ play.js
@@ -2,5 +2,29 @@
 
 const connect = require ('./client');
 
-console.log('Connecting ...');
+const handleUserInput = (key) => {
+  if(key === '\u0003') {  // Exit with ctrl c
+    console.log('Goodbye!');
+    process.exit();
+  } else {
+    return key;
+  }
+};
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+ const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  
+  stdin.on('data', data => {
+    return handleUserInput(data);
+  });
+}
+
+console.log('Connecting ...');clearInterval
 connect();
+setupInput();
